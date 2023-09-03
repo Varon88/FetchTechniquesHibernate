b7b833a900db6f9c55a7391f445e973755248637
Varon88 <nethan1226@gmail.com>
2023-09-03 23:31:13 +0530

    fully functional version of one to many relationship

--- src/main/java/org/example/Enter.java
+++ src/main/java/org/example/Enter.java
@@ -9,20 +9,6 @@ public class Enter {
 
     public static void main(String[] args){
 
-        Cars car = new Cars();
-        car.setId(0);
-        car.setName("Merecdes SLS AMG");
-
-        Cars car1 = new Cars();
-        car1.setId(1);
-        car1.setName("Ferrari 812 SuperFast");
-
-        Person person = new Person();
-        person.setId(0);
-        person.setName("Varon");
-        person.getCars().add(car);
-        person.getCars().add(car1);
-
 
         Configuration configuration = new Configuration().configure().addAnnotatedClass(Person.class).addAnnotatedClass(Cars.class);
         SessionFactory sessionFactory = configuration.buildSessionFactory();
@@ -30,6 +16,24 @@ public class Enter {
 
 
         Transaction transaction = session.beginTransaction();
+
+        Cars car = new Cars();
+        car.setId(2);
+        car.setName("Mercedes AMG ONE");
+
+        Cars car1 = new Cars();
+        car1.setId(3);
+        car1.setName("Ferrari 458 Speciale");
+
+        Person person = new Person();
+        person.setId(1);
+        person.setName("bob");
+        person.getCars().add(car);
+        person.getCars().add(car1);
+
+        //pass in the person as an object to cars inorder to display person based objects.
+        car.setPerson(person);
+        car1.setPerson(person);
 
         session.save(car);
         session.save(car1);
